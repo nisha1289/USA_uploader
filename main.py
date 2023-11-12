1f8b12fbc202c12bcc243b6c4f7f9dd1d6dfba98
Anand <147962898+nisha1289@users.noreply.github.com>
2023-11-12 05:55:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -835,7 +835,7 @@ async def account_login(bot: Client, m: Message):
     await m.reply_text("Done")
 
 
-@bot.on_message(filters.command(["txt"])
+@bot.on_message(filters.command(["txt"]))
 async def account_login(bot: Client, m: Message):
     
     editable = await m.reply_text("Send txt file")
