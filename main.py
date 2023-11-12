a4e9632cd6dae0472bd13ae48d15760e7ed162db
Anand <147962898+nisha1289@users.noreply.github.com>
2023-11-12 06:00:21 +0530

    Update main.pyl

--- main.py
+++ main.py
@@ -838,10 +838,10 @@ async def account_login(bot: Client, m: Message):
 @bot.on_message(filters.command(["txt"]))
 async def account_login(bot: Client, m: Message):
     
-    editable = await m.reply_text("Send txt file")
-    input: Message = await bot.listen(editable.chat.id)
-        x = await input.download()
-    await input.delete(True)
+       editable = await m.reply_text("Send txt file")
+       input: Message = await bot.listen(editable.chat.id)
+       x = await input.download()
+       await input.delete(True)
 
         path = f"./downloads/"
 
